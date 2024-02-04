87d7f46aff5e69d73248fd944863bbe60abc478a
MINHYUK-SWEETHO\user <alsgurdk22@gmail.com>
2024-02-04 20:18:45 +0900

    [FE] 레이아웃 - sidebar

---
+++ components/layout/sidebar.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { Montserrat } from "next/font/google";
+import { LayoutDashboard, FileCode2, MessageSquare, ImageIcon, VideoIcon, Music, Code, Settings } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+
+const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
+
+const routes = [
+  {
+    label: "Home",
+    icon: LayoutDashboard,
+    href: "/home",
+    color: "text-sky-500",
+  },
+  {
+    label: "HTML 생성",
+    icon: FileCode2,
+    href: "/htmlcode",
+    color: "text-orange-700",
+  },
+  {
+    label: "CHAT GPT",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-violet-500",
+  },
+  {
+    label: "이미지 생성",
+    icon: ImageIcon,
+    href: "/draw",
+    color: "text-pink-700",
+  },
+  // {
+  //   label: "Video Generation",
+  //   icon: VideoIcon,
+  //   href: "/video",
+  //   color: "text-orange-700",
+  // },
+  // {
+  //   label: "Music Generation",
+  //   icon: Music,
+  //   href: "/music",
+  //   color: "text-emerald-500",
+  // },
+  // {
+  //   label: "Code Generation",
+  //   icon: Code,
+  //   href: "/code",
+  //   color: "text-green-700",
+  // },
+  {
+    label: "관리자",
+    icon: Settings,
+    href: "/settings",
+  },
+];
+
+const Sidebar = () => {
+  const pathName = usePathname();
+  return (
+    <div className='space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'>
+      <div className='px-3 py-2 flex-1'>
+        <Link href='/home' className='flex items-center pl-3 mb-14'>
+          <div className='relative w-8 h-8 mr-4'>
+            <Image fill alt='Logo' src='/logo.png' />
+          </div>
+          <h1 className={ cn('text-2xl font-bold', montserrat.className) }>
+            JARVIS
+          </h1>
+        </Link>
+        <div className='space-y-1'>
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className={cn('text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+              pathName === route.href ? 'text-white bg-white/10' : 'text-zinc-400'
+              )}
+            >
+              <div className='flex items-center flex-1'>
+                <route.icon className={cn('h-5 w-5 mr-3', route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
